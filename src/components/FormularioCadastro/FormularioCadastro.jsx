ffa234a80cea96e9431f57fb7ca686b2add68428
Claud10matheus <claudiom.pst@gmail.com>
2023-09-10 15:16:32 -0300

    facilitando prototipacao com material-ui

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,9 +1,25 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
 function FormularioCadastro () {
     return (
         <form>
+            <label>Nome</label>
             <input type='text' />
+        
+            <label>Sobrenome</label>
+            <input type='text' />
+        
+            <label>CPF</label>
+            <input type='text' />
+        
+            <label>Promoções</label>
+            <input type='checkbox' />
+        
+            <label>Novidades</label>
+            <input type='checkbox' />
+
+            <Button variant='contained' color='primary' type='submit'>Confirmar</Button>
         </form>
     )
 }
