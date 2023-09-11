83762b980ae7eeb53677ce1b0dc8157fbc30aeb9
Claud10matheus <claudiom.pst@gmail.com>
2023-09-11 11:30:09 -0300

    assincronicidade e validacao

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,41 +1,67 @@
 import { Button, TextField, Switch, FormControlLabel } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 
 function FormularioCadastro() {
+
+    const [nome, setNome] = useState("")
+    const [sobrenome, setSobrenome] = useState("")
+    const [cpf, setCpf] = useState("")
+
     return (
-        <form>
-            <TextField 
-                id="nome" 
-                label="Nome" 
-                variant="outlined" 
-                margin='normal' 
-                fullWidth 
-            />
+        <form
+            onSubmit={(event) => {
+                event.preventDefault()
+                console.log({ nome: nome, sobrenome: sobrenome, cpf: cpf })
+                setNome("")
+            }}
+        >
 
-            <TextField 
-                id="sobrenome" 
-                label="Sobrenome" 
+            <TextField
+                id="nome"
+                label="Nome"
                 variant="outlined"
-                margin='normal' 
-                fullWidth 
+                margin='normal'
+                fullWidth
+                value={nome}
+                onChange={(event) => {
+                    let tmpnome = event.target.value
+                    if (tmpnome.length >= 3) {
+                        setNome(tmpnome.substr(0, 3))
+                    }
+                    setNome(tmpnome)
+                }}
             />
 
-            <TextField 
-                id="cpf" 
-                label="CPF" 
-                variant="outlined" 
-                margin='normal' 
-                fullWidth 
+            <TextField
+                id="sobrenome"
+                label="Sobrenome"
+                variant="outlined"
+                margin='normal'
+                fullWidth
+                value={sobrenome}
+                onChange={(event) => {
+                    setSobrenome(event.target.value)
+                }}
             />
 
-            <FormControlLabel 
-                label="Promoções" 
-                control={<Switch name='promocoes' defaultChecked color='primary' />}    
+            <TextField
+                id="cpf"
+                label="CPF"
+                variant="outlined"
+                margin='normal'
+                fullWidth
+                value={cpf}
+                onChange={(event) => setCpf(event.target.value)}
             />
 
-            <FormControlLabel 
-                label="Novidades" 
-                control={<Switch name='novidades' defaultChecked color='primary' />} 
+            <FormControlLabel
+                label="Promoções"
+                control={<Switch name='promocoes' defaultChecked color='primary' />}
+            />
+
+            <FormControlLabel
+                label="Novidades"
+                control={<Switch name='novidades' defaultChecked color='primary' />}
             />
 
             <Button variant='contained' color='primary' type='submit'>Confirmar</Button>
