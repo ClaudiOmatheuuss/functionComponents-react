cf1ef86c13a70bde778383d368ef52bb3945bcd9
Claud10matheus <claudiom.pst@gmail.com>
2023-09-11 16:10:05 -0300

    finalizando validacao

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,14 @@
 import { Button, TextField, Switch, FormControlLabel } from '@mui/material';
 import React, { useState } from 'react';
 
-function FormularioCadastro({ aoEnviar }) {
+function FormularioCadastro({ aoEnviar, validarCPF }) {
 
     const [nome, setNome] = useState("")
     const [sobrenome, setSobrenome] = useState("")
     const [cpf, setCpf] = useState("")
     const [promocoes, setPromocoes] = useState(true)
     const [novidades, setNovidades] = useState(false)
+    const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
 
     return (
         <form
@@ -45,15 +46,22 @@ function FormularioCadastro({ aoEnviar }) {
             />
 
             <TextField
+                onChange={(event) => {
+                    setCpf(event.target.value)
+                }}
+
+                onBlur={(event) => {
+                    const ehValido = validarCPF(event.target.value)
+                    setErros({cpf:ehValido})
+                }}
+                error={!erros.cpf.valido}
+                helperText={erros.cpf.texto}
                 id="cpf"
                 label="CPF"
                 variant="outlined"
                 margin='normal'
                 fullWidth
                 value={cpf}
-                onChange={(event) => {
-                    setCpf(event.target.value)
-                }}
             />
 
             <FormControlLabel
@@ -80,7 +88,11 @@ function FormularioCadastro({ aoEnviar }) {
                         />}
             />
 
-            <Button variant='contained' color='primary' type='submit'>Confirmar</Button>
+            <Button 
+                variant='contained' 
+                color='primary'
+                type='submit'
+            >Confirmar</Button>
         </form>
     )
 }
