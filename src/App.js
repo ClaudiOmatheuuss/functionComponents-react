f313d804fa722e0b02d35efa03a7202570daa27a
Claud10matheus <claudiom.pst@gmail.com>
2023-09-11 22:17:46 -0300

    adicionando nova validacao

--- src/App.js
+++ src/App.js
@@ -23,9 +23,22 @@ function aoEnviarForm(dados) {
 }
 
 function validarCPF(cpf) {
+  
+  function caracteresIguais (cpf) {
+    const primeiroElemento = cpf[0]
 
-  if( cpf.length != 11 ) {
+    for (let i = 1; i < cpf.length; i++) {
+      if (cpf[i] !== primeiroElemento) {
+        return false 
+      }
+    }
+    return true
+  }
+  
+  if( cpf.length !== 11 ) {
     return {valido:false, texto:"CPF deve ter 11 digitos."}
+  } else if ( caracteresIguais(cpf) === true ) {
+    return {valido:false, texto:"CPF não pode ter todos os números repetidos."}
   } else {
     return {valido: true, texto:""}
   }
