0a771e18912857a8743e12215f0d125d3d0b2847
Claud10matheus <claudiom.pst@gmail.com>
2023-09-15 00:51:47 -0300

    adicionando validacao de caracteres especiais

--- src/components/CaixaForm/validarCPF.js
+++ src/components/CaixaForm/validarCPF.js
@@ -11,10 +11,26 @@ export default function validarCPF(cpf) {
       return true
     }
     
+    function caracteresEspeciais(cpf) {
+      const caracteresEspeciais = ["!", "@", "#", "$", "%", "¨", "&", "*", "(", ")", "-", "_", ",", ".", "=", "+", "`", "[", "{", "]", "}", "<", ">", ":", ";", "/", "?", "|", "'", '"']
+  
+      let tamanhoCpf = cpf.length
+  
+      for (let i = 0; i < tamanhoCpf; i++) {
+        if (caracteresEspeciais.includes(cpf[i])) {
+          return true
+        }
+      }
+  
+      return false
+    }
+    
     if( cpf.length !== 11 ) {
       return {valido:false, texto:"CPF deve ter 11 digitos."}
     } else if ( caracteresIguais(cpf) === true ) {
       return {valido:false, texto:"CPF não pode ter todos os números repetidos."}
+    } else if ( caracteresEspeciais(cpf) ) {
+      return {valido: false, texto:"Ops! nada de caracteres especiais aqui. Apenas números."}
     } else {
       return {valido: true, texto:""}
     }
